6fcd769254ac76890c2195b09c3b1b4da2f64543
Cano23& <104598388+CanoYair@users.noreply.github.com>
2023-01-07 14:32:46 -0600

    Add files via upload

---
+++ bs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+yay -S qtile picom rofi nitrogen xorg-server-xephyr lxappearance-gtk3 megasync python-psutil brave-browser alacritty bat playerctl pulseaudio-ctl dunst alacritty fish starship neovim pavucontrol flameshot
+
+
+
+
